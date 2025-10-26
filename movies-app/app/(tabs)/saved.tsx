2909158edd0c588c8e20ec30afb738529c2a459e
Arman Bhattacharjee <66437768+quacky20@users.noreply.github.com>
2025-10-26 21:41:45 +0530

    feat: add home page feat: add search page feat: add mockup pages

---
+++ movies-app/app/(tabs)/saved.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Image } from 'react-native'
+import { icons } from '@/constants/icons'
+
+const saved = () => {
+  return (
+    <View className='bg-primary flex-1 px-10'>
+      <View className='flex justify-center items-center flex-1 flex-col gap-5'>
+        <Image source={icons.save} className='size-10' tintColor="#FFF" />
+        <Text className='text-gray-500 text-base'>Saved</Text>
+      </View>
+    </View>
+  )
+}
+
+export default saved
